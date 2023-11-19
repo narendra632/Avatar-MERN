2b7c81591c8767b2b53df5bf96638d6b94668711
Narendra Singh Rathore <mrperfectth7@gmail.com>
2023-11-19 22:40:08 +0530

    feat: basic setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
